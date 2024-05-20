73e16e844b22a3fe10e4e2c6bb132b350db8a7cd
jacob <jacobmuglia1@gmail.com>
2024-05-19 21:54:41 -0400

    refactored strategy development

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -2,7 +2,25 @@ import pandas as pd
 import numpy as np
 from arch import arch_model
 import pandas_ta
-from data.data import getdata
+import yfinance as yf
+
+#get SP500 data
+def getdata():
+    #get SP500 ticker sybols from wikipedia
+    sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+    sp500['Symbol'] = sp500['Symbol'].str.replace('.','-')
+    symbols_list = sp500['Symbol'].unique().tolist()
+    end_date = '2024-01-01'
+    start_date = pd.to_datetime(end_date) - pd.DateOffset(5*365)
+
+    #download tickers from Yahoo Finance
+    data = yf.download(tickers=symbols_list,end=end_date, start=start_date)
+
+    #filter data for top 150 trading volume
+    data['dollar_volume'] = (data.loc[:, 'dollar_volume'].unstack('ticker').rolling(5*12, min_periods=12).mean().stack())
+    data['dollar_vol_rank'] = (data.groupby('date')['dollar_volume'].rank(ascending=False))
+    data = data[data['dollar_vol_rank']<150].drop(['dollar_volume', 'dollar_vol_rank'], axis=1)
+    return data
 
 data = getdata()
 
