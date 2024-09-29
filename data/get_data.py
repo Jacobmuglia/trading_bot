9f46aedbbe0a05350d0e290d9f622c87f1ba0620
Jacobmuglia <86903988+Jacobmuglia@users.noreply.github.com>
2024-09-28 20:39:18 -0400

    Update get_data.py

--- data/get_data.py
+++ data/get_data.py
@@ -2,9 +2,9 @@ import pandas as pd
 import yfinance as yf
 
 #get wikipedia dataset
-def getdata(wiki):
+def getdata():
     #get ticker sybols from wikipedia
-    sp500 = pd.read_html()[0]
+    sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
     sp500['Symbol'] = sp500['Symbol'].str.replace('.','-')
     symbols_list = sp500['Symbol'].unique().tolist()
     end_date = '2024-01-01'
@@ -12,3 +12,5 @@ def getdata(wiki):
 
     #download tickers from Yahoo Finance
     data = yf.download(tickers=symbols_list,end=end_date, start=start_date)
+
+getdata()
